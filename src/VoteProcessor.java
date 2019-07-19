b0174bb9208c5f6b4d60e5eef09f460d9e057882
league <league@imac-17.attlocal.net>
2019-07-18 18:28:12 -0700

    finished level 3! :smile:

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
+
+	public Object calculateElectionWinner(ArrayList<String> votes) {
+		int ps = 0;
+		int es = 0;
+		for (int i = 0; i < votes.size(); i++) {
+			if (votes.get(i).equalsIgnoreCase("pope francis")) {
+				ps++;
+			}
+			else {
+				es++;
+			}
+		}
+		if(ps> es) {
+		return "pope francis";}
+		else if(ps== es) {
+			return "TIE";
+		}
+		else {
+			return "edward snowden";
+		}
+	}
 
 }
